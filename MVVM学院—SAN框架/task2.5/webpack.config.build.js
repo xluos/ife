7a89565e85fd76f5f7dff06081d59279e5358e0c
unknown <email@xluos.com>
2018-05-12 17:33:38 +0800

    重新构建项目大大减少了main.js的体积

---
+++ MVVM学院—SAN框架/task2.5/webpack.config.build.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/main.js',
+  },
+  module: {
+    rules: [
+      {
+          test: /\.html$/,
+          use: {
+              loader: "html-loader",
+              options: {minimize: true}
+          }
+      },
+      {
+          test: /\.san$/,
+          use: ['san-loader']
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          // {loader: 'svg-inline-loader'},
+          // {loader: 'file-loader'},
+          {loader: 'url-loader'}
+        ]
+      }
+  ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: "./src/main.html"
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
